ed64cd426596d3f48adaedb5798b51a444ab8e7f
Gtstride <lewisagho47@gmail.com>
2019-05-30 23:36:03 +0100

    Add animal database table

--- public/views/dashBoard.js
+++ public/views/dashBoard.js
@@ -24,7 +24,58 @@
                   }
               });
         });
-});
+
+        $("#btn-login").on('click', function(){
+
+          let userdetail = {
+            "email": $('#email').val(),
+            "password":$('#password').val(),
+          } 
+          $.ajax({
+                url: 'http://localhost:3000/users',
+                dataType: 'json',
+                type: 'GET',
+                contentType: 'application/json',
+                data: JSON.stringify(userdetail),
+                success: function( data ){
+                    $('#response').html( JSON.stringify( data ));
+                    window.location.replace("http://localhost:3000/views/dashBoard.html");
+                  //   console.log(data)
+                },
+              //   jqXhr, textStatus, errorThrown 
+                error: function(err){
+                  //  alert( errorThrown );
+                  console.log('error', err)
+                }
+            });
+      });
+
+        $("#upload").on('click', function(){
+
+          let animal = {
+              "animal-name": $('#name-of-animal').val(),
+              "species-of-animal":$('#species-of-animal').val(),
+              "history":$('#brief-history').val()
+          } 
+          $.ajax({
+                url: 'http://localhost:3000/animals',
+                dataType: 'json',
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify(animal),
+                success: function( data ){
+                    $('#response').html( JSON.stringify( data ));
+                    window.location.replace("http://localhost:3000/views/dashBoard.html");
+                  //   console.log(data)
+                },
+              //   jqXhr, textStatus, errorThrown 
+                error: function(err){
+                  //  alert( errorThrown );
+                  console.log('error', err)
+                }
+            });
+      });
+})
 
 // function checkUser(email){
 //     let alreadyAUser = 0;
